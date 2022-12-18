11dc08613248bb3afe64d4d2404f23a3fb828a9a
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2022-12-18 19:46:49 +0900

    첫번째 커밋 스프링 시큐리티 설정 및 회원가입 완료

---
+++ security1/src/main/java/com/newbietop/security1/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.newbietop.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	//해당 메서드의 리턴되는 오브젝트를 ioc로 등록된다.
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeHttpRequests()
+		  .requestMatchers("/user/**").authenticated()
+		  .requestMatchers("/manager/**").hasAnyRole("ROLE_ADMIN","ROLE_MANAGER")
+		  .requestMatchers("/admin/**").hasRole("ROLE_ADMIN")
+		  .anyRequest().permitAll()
+		  .and()
+		  .formLogin()
+		  .loginPage("/loginForm");
+		
+	    return http.build();
+	}
+}
---
+++ security1/src/main/java/com/newbietop/security1/controller/IndexController.java
@@ -0,0 +1,74 @@
+package com.newbietop.security1.controller;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.newbietop.security1.model.User;
+import com.newbietop.security1.repository.UserRepository;
+
+@Controller
+public class IndexController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@GetMapping({"","/"})
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "유저 페이지입니다.";
+	}
+
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "어드민 페이지입니다.";
+	}
+
+	@GetMapping("/manager")
+	public @ResponseBody String manager() {
+		return "매니저 페이지입니다.";
+	}
+
+	//스프링시큐리티 해당주소를 낚아챈다!
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
+	}
+	
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
+	}
+
+	@PostMapping("/join")
+	public @ResponseBody ResponseEntity<?> join(User user) {
+		System.out.println(user);
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		
+		userRepository.save(user); //패스워드를 암호화하지 않아서 시큐리티로 로그인 불가!
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(URI.create("/loginForm"));
+		
+		return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+	}
+
+}
