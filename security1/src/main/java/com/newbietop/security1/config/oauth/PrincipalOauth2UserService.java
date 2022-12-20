11a0d70e78955fe13782548f97913f18218537f2
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2022-12-20 21:15:54 +0900

    security에서 oauth를 활용한 구글 회원가입 프로필 정보 받아보기

---
+++ security1/src/main/java/com/newbietop/security1/config/oauth/PrincipalOauth2UserService.java
@@ -0,0 +1,24 @@
+package com.newbietop.security1.config.oauth;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
+
+	//구글로 부터 받은 userRequest 데이터에 대한 후처리되는 함수!
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		System.out.println("userRequest : "+userRequest.getAccessToken().getTokenValue());
+		System.out.println("userRequest : "+userRequest.getClientRegistration());
+		System.out.println("userRequest : "+userRequest.getClientRegistration().getClientName());
+		System.out.println("userRequest : "+userRequest.getClientRegistration().getClientId());
+		System.out.println("userRequest : "+super.loadUser(userRequest).getAttributes());
+		
+		//회원가입을 강제로 진행할 예정
+		return super.loadUser(userRequest);
+	}
+}
